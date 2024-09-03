4c524b4768c4af6344f2b6f3bbf3036c5477d011
SayaUSB <91643096+SayaUSB@users.noreply.github.com>
2024-09-03 18:12:59 +0700

    Delete PR 1 directory

--- PR 1/Soal 4.py
+++
@@ -1,9 +0,0 @@
-# Perbandingan USD ke Euro
-euro = 0.9
-
-# Input USD
-dollar = float(input("USD : "))
-
-# Output
-output = euro*dollar
-print("USD --> Euro : %.2f" %output)
--- PR 1/Soal 5.py
+++
@@ -1,9 +0,0 @@
-# Input banyaknya infus dalam mL dan waktu dalam menit
-infus = float(input("Volume to be infused (ml) => "))
-menit = float(input("Minutes over which to infuse => "))
-jam = menit/60
-
-#Output
-print("VTBI : %.2f" %infus)
-rate = infus/jam
-print("Rate : %.2f mL/hr" %rate)
